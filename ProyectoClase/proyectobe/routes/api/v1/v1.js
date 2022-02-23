41dfc8b0e3efdaf0381d72c6318d6f4e6d04351e
kjmg05 <kenj_martinezg@unicah.edu>
2022-02-22 18:54:14 -0600

    Feb 22. Seguridad

--- ProyectoClase/proyectobe/routes/api/v1/v1.js
+++ ProyectoClase/proyectobe/routes/api/v1/v1.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const {verifyApiHeaderToken} = require('./headerVerifyMiddleware');
+const {passport, jwtMiddleware} = require('./seguridad/jwtHelper');
 const pacientesRoutes = require('./pacientes/pacientes');
 const expedientesRoutes = require('./expedientes/expedientes');
+const seguridadRoutes = require('./seguridad/seguridad');
 
+router.use(passport.initialize());
+//public
+router.use('/seguridad', seguridadRoutes);
 //middlewares
-router.use('/pacientes', verifyApiHeaderToken, pacientesRoutes);
+router.use('/pacientes', verifyApiHeaderToken, jwtMiddleware, pacientesRoutes);
 router.use('/expedientes', expedientesRoutes);
 
 module.exports = router;
