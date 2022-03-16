23c2b2360f47439163bc0e962ed367db9ef1d0e0
kjmg05 <kenj_martinezg@unicah.edu>
2022-03-15 18:34:39 -0600

    Cors

--- ProyectoClase/proyectobe/app.js
+++ ProyectoClase/proyectobe/app.js
@@ -2,14 +2,27 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
+var whiteList = (process.env.CORS_ORIGIN || '').split(',');
+var corsOptions = {
+    origin: (origin, callback) => {
+        if(whiteList.indexOf(origin) >= 0){
+            callback(null, true)
+        } else {
+            callback(new Error('Cors not allowed'));
+        }
+    }
+}
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var apiRouter = require('./routes/api/api');
 
+
 var app = express();
 
 app.use(logger('dev'));
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
